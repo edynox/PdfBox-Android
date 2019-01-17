381f90ec6bdeb9afb8bfde05f6659ed68cccc163
Eduard Čuba <cuba@procivis.ch>
2019-01-17 10:45:25 +0100

    Implement a digital signature example

--- sample/src/main/java/com/tom_roush/pdfbox/sample/MainActivity.java
+++ sample/src/main/java/com/tom_roush/pdfbox/sample/MainActivity.java
@@ -2,309 +2,236 @@ package com.tom_roush.pdfbox.sample;
 
 import android.app.Activity;
 import android.content.res.AssetManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.view.Menu;
+import android.util.Base64;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import com.github.barteksc.pdfviewer.PDFView;
 import com.tom_roush.pdfbox.pdmodel.PDDocument;
-import com.tom_roush.pdfbox.pdmodel.PDDocumentCatalog;
-import com.tom_roush.pdfbox.pdmodel.PDPage;
-import com.tom_roush.pdfbox.pdmodel.PDPageContentStream;
-import com.tom_roush.pdfbox.pdmodel.encryption.AccessPermission;
-import com.tom_roush.pdfbox.pdmodel.encryption.StandardProtectionPolicy;
-import com.tom_roush.pdfbox.pdmodel.font.PDFont;
-import com.tom_roush.pdfbox.pdmodel.font.PDType1Font;
-import com.tom_roush.pdfbox.pdmodel.graphics.image.JPEGFactory;
-import com.tom_roush.pdfbox.pdmodel.graphics.image.LosslessFactory;
-import com.tom_roush.pdfbox.pdmodel.graphics.image.PDImageXObject;
-import com.tom_roush.pdfbox.pdmodel.interactive.form.PDAcroForm;
-import com.tom_roush.pdfbox.pdmodel.interactive.form.PDCheckbox;
-import com.tom_roush.pdfbox.pdmodel.interactive.form.PDComboBox;
-import com.tom_roush.pdfbox.pdmodel.interactive.form.PDField;
-import com.tom_roush.pdfbox.pdmodel.interactive.form.PDListBox;
-import com.tom_roush.pdfbox.pdmodel.interactive.form.PDRadioButton;
-import com.tom_roush.pdfbox.pdmodel.interactive.form.PDTextField;
-import com.tom_roush.pdfbox.rendering.PDFRenderer;
-import com.tom_roush.pdfbox.text.PDFTextStripper;
-import com.tom_roush.pdfbox.util.PDFBoxResourceLoader;
+import com.tom_roush.pdfbox.pdmodel.interactive.digitalsignature.PDSignature;
+import com.tom_roush.pdfbox.pdmodel.interactive.digitalsignature.SignatureInterface;
 
-import org.spongycastle.jce.provider.BouncyCastleProvider;
+import org.spongycastle.cert.jcajce.JcaCertStore;
+import org.spongycastle.cms.CMSException;
+import org.spongycastle.cms.CMSSignedData;
+import org.spongycastle.cms.CMSSignedDataGenerator;
 
+import org.spongycastle.cms.SignerInfoGenerator;
+import org.spongycastle.cms.jcajce.JcaSignerInfoGeneratorBuilder;
+import org.spongycastle.operator.ContentSigner;
+import org.spongycastle.operator.DigestCalculatorProvider;
+import org.spongycastle.operator.OperatorCreationException;
+import org.spongycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.spongycastle.operator.jcajce.JcaDigestCalculatorProviderBuilder;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.Security;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Calendar;
+import java.util.Collections;
 
-public class MainActivity extends Activity {
-	File root;
-	AssetManager assetManager;
-	Bitmap pageImage;
-	TextView tv;
+public class MainActivity extends Activity implements SignatureInterface {
 
-	@Override
+  File root;
+  PDDocument document;
+  PDFView pdfView;
+  Integer pageNumber = 0;
+  AssetManager assetManager;
+
+
+  @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    setContentView(R.layout.activity_main);
+    root = android.os.Environment.getExternalStorageDirectory();
+    pdfView = findViewById(R.id.pdfView);
+    assetManager = getAssets();
+    downloadPdf(null);
+  }
 
-	@Override
-	protected void onStart() {
-		super.onStart();
-		setup();
-	}
+  protected void makeToast(String message) {
+    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+  }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
+	// load a custom PDF
+	protected void getRemotePdf() {
+    try {
+      URL url = new URL("https://vetri.global/wp-content/themes/vetri/documents/whitepaper.pdf");
+      URLConnection conn = url.openConnection();
 
-	/**
-	 * Initializes variables used for convenience
-	 */
-	private void setup() {
-		// Enable Android-style asset loading (highly recommended)
-		PDFBoxResourceLoader.init(getApplicationContext());
-		// Find the root of the external storage.
-		root = android.os.Environment.getExternalStorageDirectory();
-		assetManager = getAssets();
-		tv = (TextView) findViewById(R.id.statusTextView);
-	}
+      int size = conn.getContentLength();
+      DataInputStream stream = new DataInputStream(url.openStream());
 
-	/**
-	 * Creates a new PDF from scratch and saves it to a file
-	 */
-	public void createPdf(View v) {
-		PDDocument document = new PDDocument();
-		PDPage page = new PDPage();
-		document.addPage(page);
+      byte[] buffer = new byte[size];
+      stream.readFully(buffer);
+      stream.close();
 
-		// Create a new font object selecting one of the PDF base fonts
-		PDFont font = PDType1Font.HELVETICA;
-		// Or a custom font
-//		try {
-//			PDType0Font font = PDType0Font.load(document, assetManager.open("MyFontFile.TTF"));
-//		} catch(IOException e) {
-//			e.printStackTrace();
-//		}
+      document = PDDocument.load(buffer);
+    } catch (IOException e) {
+      makeToast("Error: " + e.getMessage());
+      e.printStackTrace();
+    }
+  }
 
-		PDPageContentStream contentStream;
+  protected void renderPdf() {
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    try {
+      document.save(out);
+      pdfView.fromBytes(out.toByteArray())
+          .defaultPage(pageNumber)
+          .enableSwipe(true)
+          .swipeHorizontal(false)
+          .load();
+    } catch (IOException e) {
+      makeToast("Error: " + e.getMessage());
+      e.printStackTrace();
+    }
+  }
 
-		try {
-			// Define a content stream for adding to the PDF
-			contentStream = new PDPageContentStream(document, page);
+  public void downloadPdf(View v) {
+    makeToast("Loading");
+    Thread thread = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        getRemotePdf();
+        renderPdf();
+      }
+    });
+    thread.start();
+  }
 
-			// Write Hello World in blue text
-			contentStream.beginText();
-			contentStream.setNonStrokingColor(15, 38, 192);
-			contentStream.setFont(font, 12);
-			contentStream.newLineAtOffset(100, 700);
-			contentStream.showText("Hello World");
-			contentStream.endText();
+  public void savePdf(View v) {
+    String path = root.getAbsolutePath() + "/Download/test.pdf";
+    try {
+      FileOutputStream out = new FileOutputStream(path);
+      document.saveIncremental(out);
+      makeToast("Saved to: " + path);
+    } catch (IOException e) {
+      makeToast("Error: " + e.getMessage());
+      e.printStackTrace();
+    }
+  }
 
-			// Load in the images
-			InputStream in = assetManager.open("falcon.jpg");
-			InputStream alpha = assetManager.open("trans.png");
+  public void signPdf(View v) {
+    PDSignature signature = new PDSignature();
+    signature.setFilter(PDSignature.FILTER_ADOBE_PPKLITE); // default filter
 
-			// Draw a green rectangle
-			contentStream.addRect(5, 500, 100, 100);
-			contentStream.setNonStrokingColor(0, 255, 125);
-			contentStream.fill();
+    // subfilter for basic and PAdES Part 2 signatures
+    signature.setSubFilter(PDSignature.SUBFILTER_ADBE_PKCS7_DETACHED);
+    signature.setName("Eduard Cuba");
+    signature.setLocation("Zurich, ZH");
+    signature.setReason("Testing");
+    signature.setSignDate(Calendar.getInstance());
 
-			// Draw the falcon base image
-			PDImageXObject ximage = JPEGFactory.createFromStream(document, in);
-			contentStream.drawImage(ximage, 20, 20);
+    try {
+      document.addSignature(signature, this);
+      makeToast("Success");
+    } catch (IOException e) {
+      makeToast("Error: " + e.getMessage());
+      e.printStackTrace();
+    }
 
-			// Draw the red overlay image
-			Bitmap alphaImage = BitmapFactory.decodeStream(alpha);
-			PDImageXObject alphaXimage = LosslessFactory.createFromImage(document, alphaImage);
-			contentStream.drawImage(alphaXimage, 20, 20 );
+  }
 
-			// Make sure that the content stream is closed:
-			contentStream.close();
+  protected PrivateKey getPrivateKey() {
 
-			// Save the final pdf document to a file
-			String path = root.getAbsolutePath() + "/Download/Created.pdf";
-			document.save(path);
-			document.close();
-			tv.setText("Successfully wrote PDF to " + path);
+    try {
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+      StringBuilder pkcs8Lines = new StringBuilder();
+      InputStream key = assetManager.open("key.pem");
 
-	/**
-	 * Loads an existing PDF and renders it to a Bitmap
-	 */
-	public void renderFile(View v) {
-		// Render the page and save it to an image file
-		try {
-			// Load in an already created PDF
-			PDDocument document = PDDocument.load(assetManager.open("Created.pdf"));
-			// Create a renderer for the document
-			PDFRenderer renderer = new PDFRenderer(document);
-			// Render the image to an RGB Bitmap
-			pageImage = renderer.renderImage(0, 1, Bitmap.Config.RGB_565);
+      BufferedReader in = new BufferedReader(new InputStreamReader(key, StandardCharsets.UTF_8));
+      String str;
 
-			// Save the render result to an image
-			String path = root.getAbsolutePath() + "/Download/render.jpg";
-			File renderFile = new File(path);
-			FileOutputStream fileOut = new FileOutputStream(renderFile);
-			pageImage.compress(Bitmap.CompressFormat.JPEG, 100, fileOut);
-			fileOut.close();
-			tv.setText("Successfully rendered image to " + path);
-			// Optional: display the render result on screen
-			displayRenderedImage();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+      while ((str=in.readLine()) != null) {
+        pkcs8Lines.append(str);
+      }
 
-	/**
-	 * Fills in a PDF form and saves the result
-	 */
-	public void fillForm(View v) {
-		try {
-			// Load the document and get the AcroForm
-			PDDocument document = PDDocument.load(assetManager.open("FormTest.pdf"));
-			PDDocumentCatalog docCatalog = document.getDocumentCatalog();
-			PDAcroForm acroForm = docCatalog.getAcroForm();
+      in.close();
 
-			// Fill the text field
-            PDTextField field = (PDTextField) acroForm.getField("TextField");
-            field.setValue("Filled Text Field");
-			// Optional: don't allow this field to be edited
-			field.setReadOnly(true);
+      // Remove the "BEGIN" and "END" lines, as well as any whitespace
 
-			PDField checkbox = acroForm.getField("Checkbox");
-			((PDCheckbox) checkbox).check();
+      String pkcs8Pem = pkcs8Lines.toString();
+      pkcs8Pem = pkcs8Pem.replace("-----BEGIN PRIVATE KEY-----", "");
+      pkcs8Pem = pkcs8Pem.replace("-----END PRIVATE KEY-----", "");
+      pkcs8Pem = pkcs8Pem.replaceAll("\\s+","");
 
-			PDField radio = acroForm.getField("Radio");
-			((PDRadioButton)radio).setValue("Second");
+      // Base64 decode the result
 
-			PDField listbox = acroForm.getField("ListBox");
-			List<Integer> listValues = new ArrayList<>();
-			listValues.add(1);
-			listValues.add(2);
-			((PDListBox) listbox).setSelectedOptionsIndex(listValues);
+      byte [] pkcs8EncodedBytes = Base64.decode(pkcs8Pem, Base64.DEFAULT);
 
-			PDField dropdown = acroForm.getField("Dropdown");
-			((PDComboBox) dropdown).setValue("Hello");
+      // extract the private key
 
-			String path = root.getAbsolutePath() + "/Download/FilledForm.pdf";
-			tv.setText("Saved filled form to " + path);
-			document.save(path);
-			document.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+      PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(pkcs8EncodedBytes);
+      KeyFactory kf = KeyFactory.getInstance("RSA");
 
-	/**
-	 * Strips the text from a PDF and displays the text on screen
-	 */
-	public void stripText(View v) {
-		String parsedText = null;
-		PDDocument document = null;
-		try {
-			document = PDDocument.load(assetManager.open("Hello.pdf"));
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
+      return kf.generatePrivate(keySpec);
+    } catch (IOException | InvalidKeySpecException | NoSuchAlgorithmException e) {
+      makeToast("Error: " + e.getMessage());
+      e.printStackTrace();
+    }
+    return null;
+  }
 
-		try {
-			PDFTextStripper pdfStripper = new PDFTextStripper();
-			pdfStripper.setStartPage(0);
-			pdfStripper.setEndPage(1);
-			parsedText = "Parsed text: " + pdfStripper.getText(document);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (document != null) document.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		tv.setText(parsedText);
-	}
+  protected Certificate getCertificate() {
+    try {
+      BufferedInputStream bis = new BufferedInputStream(assetManager.open("cert.pem"));
+      CertificateFactory cf = CertificateFactory.getInstance("X.509");
 
-	/**
-	 * Creates a simple pdf and encrypts
-	 */
-	public void createEncryptedPdf()
-	{
-		String path = root.getAbsolutePath() + "/Download/crypt.pdf";
+      return cf.generateCertificate(bis);
+    } catch (IOException | CertificateException e) {
+      makeToast("Error: " + e.getMessage());
+      e.printStackTrace();
+    }
+    return null;
+  }
 
-		int keyLength = 128; // 128 bit is the highest currently supported
 
-		// Limit permissions of those without the password
-		AccessPermission ap = new AccessPermission();
-		ap.setCanPrint(false);
+  @Override
+  public byte[] sign(InputStream content) throws IOException {
 
-		// Sets the owner password and user password
-		StandardProtectionPolicy spp = new StandardProtectionPolicy("12345", "hi", ap);
+    CMSSignedDataGenerator gen = new CMSSignedDataGenerator();
+    X509Certificate cert = (X509Certificate) getCertificate();
+    PrivateKey pk = getPrivateKey();
 
-		// Setups up the encryption parameters
-		spp.setEncryptionKeyLength(keyLength);
-		spp.setPermissions(ap);
-		BouncyCastleProvider provider = new BouncyCastleProvider();
-		Security.addProvider(provider);
+    try {
+      ContentSigner sha1Signer = new JcaContentSignerBuilder("SHA256WithRSA").build(pk);
+      DigestCalculatorProvider dcp = new JcaDigestCalculatorProviderBuilder().build();
+      SignerInfoGenerator sig = new JcaSignerInfoGeneratorBuilder(dcp).build(sha1Signer, cert);
+      JcaCertStore certStore = new JcaCertStore(Collections.singletonList(cert));
 
-		PDFont font = PDType1Font.HELVETICA;
-		PDDocument document = new PDDocument();
-		PDPage page = new PDPage();
+      gen.addSignerInfoGenerator(sig);
+      gen.addCertificates(certStore);
 
-		document.addPage(page);
+      CMSProcessableInputStream msg = new CMSProcessableInputStream(content);
+      CMSSignedData signedData = gen.generate(msg, false);
 
-		try
-		{
-			PDPageContentStream contentStream = new PDPageContentStream(document, page);
+      return signedData.getEncoded();
 
-			// Write Hello World in blue text
-			contentStream.beginText();
-			contentStream.setNonStrokingColor(15, 38, 192);
-			contentStream.setFont(font, 12);
-			contentStream.newLineAtOffset(100, 700);
-			contentStream.showText("Hello World");
-			contentStream.endText();
-			contentStream.close();
-
-			// Save the final pdf document to a file
-			document.protect(spp); // Apply the protections to the PDF
-			document.save(path);
-			document.close();
-
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * Helper method for drawing the result of renderFile() on screen
-	 */
-	private void displayRenderedImage() {
-		new Thread() {
-			public void run() {
-				runOnUiThread(new Runnable() {
-					@Override
-					public void run() {
-						ImageView imageView = (ImageView) findViewById(R.id.renderedImageView);
-						imageView.setImageBitmap(pageImage);
-					}
-				});
-			}
-		}.start();
-	}
+    } catch (OperatorCreationException | CertificateEncodingException | CMSException e) {
+      makeToast("Error: " + e.getMessage());
+      e.printStackTrace();
+      return null;
+    }
+  }
 }
